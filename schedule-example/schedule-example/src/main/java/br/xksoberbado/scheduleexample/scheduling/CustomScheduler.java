d966610d82607e03a2016bb7c03236c67ce56dd5
Igor Rudel <igor.rudel@realizecfi.com.br>
2024-09-05 11:20:25 -0300

    --

--- schedule-example/schedule-example/src/main/java/br/xksoberbado/scheduleexample/scheduling/CustomScheduler.java
+++ schedule-example/schedule-example/src/main/java/br/xksoberbado/scheduleexample/scheduling/CustomScheduler.java
@@ -1,6 +1,7 @@
 package br.xksoberbado.scheduleexample.scheduling;
 
 import io.micronaut.scheduling.annotation.Scheduled;
+import io.micronaut.scheduling.annotation.Schedules;
 import jakarta.inject.Singleton;
 import lombok.extern.slf4j.Slf4j;
 
@@ -8,8 +9,23 @@ import lombok.extern.slf4j.Slf4j;
 @Singleton
 public class CustomScheduler {
 
-    @Scheduled(cron = "*/15 * * * * *")
-    public void run() {
-        log.info("Thread: {}", Thread.currentThread().threadId());
+    @Scheduled(cron = "*/10 * * * * *")
+    public void cron() {
+        log.info("Cron: {}", Thread.currentThread().threadId());
+    }
+
+    @Scheduled(fixedDelay = "10s")
+    public void fixedDelay() {
+        log.info("Fixed Delay: {}", Thread.currentThread().threadId());
+    }
+
+    @Schedules(
+        value = {
+            @Scheduled(fixedDelay = "2s"),
+            @Scheduled(fixedDelay = "10s")
+        }
+    )
+    public void schedules() {
+        log.info("Schedules: {}", Thread.currentThread().threadId());
     }
 }
